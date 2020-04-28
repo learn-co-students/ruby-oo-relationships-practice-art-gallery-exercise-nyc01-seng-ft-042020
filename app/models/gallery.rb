ebf2e197737fd36d99ff6741a187951b71332687
Joshua <jykim92@gmail.com>
2020-04-27 22:16:15 -0400

    Done.

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,10 +1,40 @@
 class Gallery
 
   attr_reader :name, :city
+  @@all = []
 
   def initialize(name, city)
     @name = name
     @city = city
+    Gallery.all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def paintings
+    Painting.all.select do |x|
+      x.gallery == self
+    end
+  end
+
+  def artists
+    paintings.map do |x|
+      x.artist
+    end
+  end
+
+  def artist_name
+    artists.map do |x|
+      x.name
+    end
+  end
+
+  def most_expensive_painting
+    Painting.all.max_by do |x|
+      x.price
+    end
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,6 +1,16 @@
 require_relative '../config/environment.rb'
 
 
+artist1 = Artist.new("Josh", 5)
+artist2 = Artist.new("Jessica", 7)
+artist3 = Artist.new("Sonia", 12)
+gallery1 = Gallery.new("The Gallery", "New York")
+gallery2 = Gallery.new("The Better Gallery", "New Jersey")
+gallery3 = Gallery.new("The Best Gallery", "South Korea")
+painting1 = Painting.new("The painting", 150, artist1, gallery1)
+painting2 = Painting.new("The real Painting", 250, artist2, gallery2)
+painting3 = Painting.new("The Real Painting Confirmed", 500, artist3, gallery3)
+
 
 
 binding.pry
