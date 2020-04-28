cc0aea63ef04f21eba4ff6416c5f4e954e386cf1
github username <github email address>
2020-04-27 21:19:20 -0400

    Done.

--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,12 +1,29 @@
 class Painting
 
-  attr_reader :title, :price
+  attr_reader :title, :price, :artist, :gallery
 
   @@all = []
 
-  def initialize(title, price)
+  def initialize(artist,title, price,gallery)
+    @artist = artist
     @title = title
     @price = price
+    @gallery = gallery
+    @@all << self 
   end
+
+
+  def self.all
+    @@all
+  end
+
+
+  def self.total_price
+    self.all.reduce(0) do |sum, painting|
+      sum + painting.price
+  end
+end
+
+
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,6 +1,13 @@
 require_relative '../config/environment.rb'
 
+artist1 = Artist.new("piccasso", 356)
+artist2 = Artist.new("Bob Ross", 40)
 
+gallery1 = Gallery.new("The Met", "NYC")
+gallery2 = Gallery.new("The new museum", "NYC")
+
+painting1 = Painting.new(artist1 ,"nice", 190432, gallery1)
+painting2 = Painting.new(artist2 ,"nice day", 19043920, gallery2)
 
 
 binding.pry
