bee788a74d21c008696e45040f995fbf9b43caa8
carolineglass <carolineglass89@gmail.com>
2020-04-28 08:56:55 -0400

    Done.

--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,12 +1,34 @@
 class Painting
 
-  attr_reader :title, :price
-
+  attr_reader :title, :price, :artist, :gallery 
+   
   @@all = []
 
-  def initialize(title, price)
+  def initialize(title, price, gallery, artist)
     @title = title
     @price = price
+    @gallery = gallery 
+    @artist = artist 
+    Painting.all << self
+  end
+  def self.all 
+    @@all
+  end 
+  def self.total_price
+    total = 0 
+      Painting.all.each do |painting|
+        total = total + painting.price
+      end 
+    total 
   end
 
 end
+
+
+# **PAINTING**
+
+#  ((* `Painting.all`
+#     * Returns an `array` of all the paintings))
+
+#   ((* `Painting.total_price`
+#     * Returns an `integer` that is the total price of all paintings))
--- tools/console.rb
+++ tools/console.rb
@@ -1,7 +1,16 @@
 require_relative '../config/environment.rb'
 
+gallery1 = Gallery.new("MOMA", "NYC")
+gallery2 = Gallery.new("Tate Modern", "London")
 
+artist1 = Artist.new("Caroline", 5)
+artist2 = Artist.new("Paul", 10)
 
+painting1 = Painting.new("Naked Woman", 300, gallery1, artist1)
+painting2 = Painting.new("Flying Monkeys", 500, gallery2, artist2)
+painting3 = Painting.new("Waterfall", 10000, gallery2, artist2)
+painting4 = Painting.new("Waterfall", 10000, gallery2, artist2)
+painting5 = Painting.new("Waterfall", 10000, gallery1, artist2)
 
 binding.pry
 
