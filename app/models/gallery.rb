ed760f9d7ee82001006eb0cd8647f9babad41376
rwilt <rosie.wilt@gmail.com>
2020-04-27 21:55:15 -0400

    Done.

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,10 +1,46 @@
 class Gallery
 
+  @@all = []
   attr_reader :name, :city
 
   def initialize(name, city)
     @name = name
     @city = city
+    Gallery.all << self
   end
+  
+  def self.all
+    @@all
+  end
+
+  def paintings
+    Painting.all.select do |painting|
+      painting.gallery == self
+    end
+  end
+
+    def artists
+      paintings.map do |painting|
+      painting.artist
+    end
+    end
+
+    def artist_names
+      paintings.map do |painting|
+      painting.artist.name
+      end
+  end
+  
+  def most_expensive_painting
+    total = 0
+    ans = []
+    paintings.each do |painting| 
+      if painting.price > total 
+      ans.push(painting)
+      end
+    end
+    ans.last
+  end
+
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,6 +1,15 @@
 require_relative '../config/environment.rb'
 
+basquiat = Artist.new("Jean-Michel Basquiat", 10)
+raymond_p = Artist.new("Raymond Pettibon", 30)
+jenny_h = Artist.new("Jenny Holzer",25)
+moma = Gallery.new("MoMA", "NYC")
+tate = Gallery.new("Tate", "London")
 
+basquiat.create_painting("Dog Leg Study", 900, moma)
+raymond_p.create_painting("No Title(Kryptonite Kisses..", 10000, moma)
+jenny_h.create_painting("Inflammatory Essays", 800, tate)
+raymond_p.create_painting("Painting", 300,moma)
 
 
 binding.pry
