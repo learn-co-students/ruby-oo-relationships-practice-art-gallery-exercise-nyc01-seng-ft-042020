b7a1a48a0dc22696c7c1f2899d0db84a21483668
ra-bluebirdgroup <ra@bluebirdgroup.ltd>
2020-04-28 23:13:13 -0400

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -2,3 +2,14 @@ require 'pry'
 require_relative '../app/models/artist.rb'
 require_relative '../app/models/gallery.rb'
 require_relative '../app/models/painting.rb'
+
+artist1 = Artist.new('Pablo', 40)
+artist2 = Artist.new('Leo', 60)
+
+gallery1 = Gallery.new('The Gallery', 'NYC')
+gallery2 = Gallery.new('The Gallery 2', 'L.A')
+
+painting1 = Painting.new('title1', 1000, gallery1, artist1)
+painting2 = Painting.new('title2', 3000, gallery2, artist2)
+
+#Artist.most_prolific
